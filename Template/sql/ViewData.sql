d7efb9bfd839a67fd24791b186d02ab64f25b0a4
Nigel Major <NigelWGMajor@users.noreply.github.com>
2024-10-03 15:36:07 -0500

    add sql to view data

---
+++ Template/sql/ViewData.sql
@@ -0,0 +1,38 @@
+use operationsLocal;
+go
+SELECT TOP (1000) [OperationFlowStateID]
+      ,[UniqueKey]
+      ,[ActivityName]
+      ,[ActivityState]
+      ,[ActivityStateName]
+      ,[Count]
+      ,[Notes]
+      ,[ProcessId]
+      ,[SequenceNumber]
+      ,[TimeStarted]
+      ,[TimeEnded]
+	  ,case when timestarted = '0001-01-01 00:00:00'
+	      then -1 
+		  else datediff(millisecond, isnull(timestarted, getutcdate()), isnull(timeended, getutcdate())) 
+	  end as Duration
+      ,[TimeUpdated]
+  from [rpt].[OperationFlowStates]
+
+select 
+  UniqueKey 
+  ,SequenceNumber 
+  ,[count]
+  ,ActivityName Activity
+  ,ActivityStateName [State] 
+  ,case when TimeStarted = '0001-01-01 00:00:00'
+	  then -1 
+	  when TimeEnded = '0001-01-01 00:00:00'
+	  then -1
+	  else datediff(millisecond, timestarted, timeended) 
+  end as Duration
+from rpt.OperationFlowStateHistory 
+order by UniqueKey, SequenceNumber 
+
+-- THESE CAN BE USED TO PURGE FINISHED FILES FROM THE MAIN TABLE AND ALL FILES FROM THE HISTORY TABLE!
+-- exec ReportsLocal.rpt.OperationFlowState_Purge;
+-- exec ReportsLocal.rpt.OperationFlowStateHistory_Purge;
